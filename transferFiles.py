10679a7f1e401f3417b6a5d7016a3f0d93370251
Yau Kwan Kiu <yaukwankiu@gmail.com>
2014-07-11 10:28:34 +0800

    adding keys to transferFiles.py

--- transferFiles.py
+++ transferFiles.py
@@ -1,15 +1,24 @@
 # from source /CWB/ folder to target /CWB/ folder
 
-source = "d:/CWB/"
-target = "f:/CWB/"
+##########################
+#   edit here: parameters
+#
+source = "f:/CWB/"
+target = "d:/CWB/"
+typeKey = "charts"
 dryRun = False
+dateKey = ""
+
+#
+##########################
+
 import os
 import shutil
 import time
 
 L   = os.listdir(source)
 L   = [v for v in L if os.path.isdir(source+v)]
-L   = [v+"/" for v in L]
+L   = [v+"/" for v in L if typeKey==v]
 
 print '\t'.join(L)
 
@@ -17,7 +26,7 @@ time.sleep(3)
 
 for typeFolder in L:
     L2 = os.listdir(source + typeFolder)
-    L2 = [v + "/" for v in L2]
+    L2 = [v + "/" for v in L2 if dateKey<=v]
     for dateFolder in L2:
         L3 = os.listdir(source+typeFolder+dateFolder)
         if L3 == []:
