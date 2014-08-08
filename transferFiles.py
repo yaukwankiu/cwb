9b7782718be8e6429200d6340a89124b0c9fc608
Yau Kwan Kiu <yaukwankiu@gmail.com>
2014-08-08 11:58:34 +0800

    fix

--- transferFiles.py
+++ transferFiles.py
@@ -8,7 +8,7 @@ import shutil
 import time
 
 L   = os.listdir(source)
-L   = [v for v in L if os.path.isdir(source+v)]
+L   = [v for v in L if os.path.isdir(source+v) and not ('.git' in v) and not ('allinone') in v]
 L   = [v+"/" for v in L]
 
 print '\t'.join(L)
@@ -16,16 +16,19 @@ print '\t'.join(L)
 time.sleep(3)
 
 for typeFolder in L:
-    L2 = os.listdir(source + typeFolder)
-    L2 = [v + "/" for v in L2]
+    L2 = os.listdir(source + typeFolder)   
+    #print L2 #debug
+    L2 = [v + "/" for v in L2 if not ('.git' in v)]
     for dateFolder in L2:
-        if dataFolder.startswith('.'):
+        if '.git' in dateFolder:
             continue # /.git/     folder
         L3 = os.listdir(source+typeFolder+dateFolder)
         if L3 == []:
             continue
         else:
             for fileName in L3:
+                #if '.git' in fileName:
+                #    continue # /.git/     folder
                 if os.path.exists(target+typeFolder+dateFolder+fileName):
                     pass
                     #print "target exists!"
